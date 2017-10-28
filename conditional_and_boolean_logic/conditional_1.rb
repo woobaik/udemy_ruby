725435387e3e61f7a9108cf25ff4fc33bcb519b5
Joungwoo <joungwoo.baik@gmail.com>
2017-10-28 13:26:44 -0700

    finishied conditioanl_1.rb

---
+++ conditional_and_boolean_logic/conditional_1.rb
@@ -0,0 +1,31 @@
+# # if - else
+# Binary decision
+# if yes, do this, if no, do that
+#
+#   Comparing the size of numbers
+
+  x = rand(100)
+  y = rand(100 )
+# Nested Conditional
+  if x > y
+    puts "#{x} is greater than #{y}."
+  else
+    if x == y
+      puts "#{x} is equal to #{y}."
+
+    else
+    puts "#{x} is less than #{y}"
+    end
+  end
+
+
+# if - elsif - else
+
+if x > y
+  puts "#{x} is greater than #{y}."
+elsif x == y
+    puts "#{x} is equal to #{y}."
+else
+  puts "#{x} is less than #{y}"
+ 
+end
