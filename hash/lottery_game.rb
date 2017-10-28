e28ca61b973cf71e0aec342039e7d8db291e9a39
Joungwoo <joungwoo.baik@gmail.com>
2017-10-28 10:36:37 -0700

    Added lottery game, Lecture done

--- hash/lottery_game.rb
+++ hash/lottery_game.rb
@@ -16,4 +16,19 @@ players = ["Duncan", "Kevan", "Tara", "Boddy", "Jane"]
 # winner = players[rand(players.length)]   # 0 <= x < 5
 
 winner = players.sample
-puts "And the winner is ... #{winner}"
+puts "And the winner is ... #{winner}!"
+
+
+players = {
+  3 => "Duncan",
+  5 => "Kevan",
+  8 => "Tara",
+  4 => "Bobby",
+  1 => "Jane"
+}
+players.default = "nobody"
+winning_number = rand(10) + 1 # 1 <= x <= 10
+
+puts "And the winner is ... #{players[winning_number]}!"
+
+#players.sample or players.shuffle!.pop would work as well
