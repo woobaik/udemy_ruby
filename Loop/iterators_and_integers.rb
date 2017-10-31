654af82e368f5ea71893ac52ed26bd200ba83748
Joungwoo <joungwoo.baik@gmail.com>
2017-10-31 00:07:02 -0700

    times, upto, range

---
+++ Loop/iterators_and_integers.rb
@@ -0,0 +1,12 @@
+# Ranges
+(3..7).each {|n| puts n}
+(-5..7).each {|n| puts n}
+
+# do some x times
+x = 9
+(1..9).each {puts "hello"}
+(0...9).each {puts "hi"}
+
+# We can actually skip the step where we create a range
+# x.upto(y) iterate over range x..y
+# x.times -> not worry about variable
