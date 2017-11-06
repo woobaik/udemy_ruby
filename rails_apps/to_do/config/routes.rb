9b434da8a863cfa61c6ee9f98acf00053c5f529b
Joungwoo <joungwoo.baik@gmail.com>
2017-11-05 20:28:34 -0800

    generated task controller, routes which map paths to controller actions

--- rails_apps/to_do/config/routes.rb
+++ rails_apps/to_do/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  get 'tasks/new'
+
   get 'welcome/index'
   root 'welcome#index'
+  resources :tasks
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
