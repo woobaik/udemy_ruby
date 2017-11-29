c6c262e28858f00cf3d084c8fc98b0f16ea51c48
Joungwoo <joungwoo.baik@gmail.com>
2017-11-29 00:01:25 -0800

    Added destroy and show for tasks

--- rails_apps/to_do/app/controllers/tasks_controller.rb
+++ rails_apps/to_do/app/controllers/tasks_controller.rb
@@ -23,6 +23,16 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
   private
   # whitelisting parameters
   def task_params
