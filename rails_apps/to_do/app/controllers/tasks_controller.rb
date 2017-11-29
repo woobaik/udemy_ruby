44cff9a3d03dca38dffa1c8afd79e87f6894b058
Joungwoo <joungwoo.baik@gmail.com>
2017-11-28 22:04:49 -0800

    created new and update for Tasks

--- rails_apps/to_do/app/controllers/tasks_controller.rb
+++ rails_apps/to_do/app/controllers/tasks_controller.rb
@@ -13,7 +13,18 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
   private
+  # whitelisting parameters
   def task_params
     params.require(:task).permit(:title, :details)
   end
