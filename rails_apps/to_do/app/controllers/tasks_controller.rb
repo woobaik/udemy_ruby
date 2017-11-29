76a32ff0f9c73241f00368d35aac364019bfca00
Joungwoo <joungwoo.baik@gmail.com>
2017-11-29 00:39:19 -0800

    added validation when the title is blank in Task page

--- rails_apps/to_do/app/controllers/tasks_controller.rb
+++ rails_apps/to_do/app/controllers/tasks_controller.rb
@@ -9,8 +9,11 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    @task.save
-    redirect_to tasks_path
+    if @task.save
+      redirect_to tasks_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -19,8 +22,11 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    @task.update(task_params)
-    redirect_to tasks_path
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render 'edit'
+    end 
   end
 
   def destroy
