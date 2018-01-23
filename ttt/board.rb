a6fb2264af35b480e0fa28609148c6b02ef2bf7f
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-01-22 16:04:57 -0800

    Updated 'board.rb' grid

--- ttt/board.rb
+++ ttt/board.rb
@@ -1,3 +1,22 @@
 class Board
+  WIDTH = 5
+  HEIGHT = WIDTH
+  attr_accessor :grid
+  def initialize
+    @grid = Array.new(HEIGHT) {Array.new(WIDTH, " ")}
+  end
 
+  def print_grid
+    @grid.each do |row|
+      puts
+      row.each do |cell|
+        print "[#{cell}]"
+      end
+    end
+    puts
+  end
 end
+
+a = Board.new
+a.grid[2][1] = "x"
+a.print_grid
